4171a5c8a32d1d52e04e343f418f5bf5194f3a65
Ubuntu <vishnu.vinod@people10.com>
2017-08-05 13:07:39 +0530

    update time sheet

--- server/api/employee/employee.controller.js
+++ server/api/employee/employee.controller.js
@@ -5,7 +5,34 @@ let controller = {
         EmployeeService.getEmployees()
             .then(employees => { res.send({ employees }) })
             .catch(next);
+    },
+    getTimeSheet(req,res, next){
+        console.log("getting timesheet");
+        EmployeeService.getTimeSheet(req.params.id)
+            .then(timesheet => { res.send({timesheet})})
+            .catch(next);
+    },
+    updateTimeSheet(req, res, next) {
+        console.log('controll');
+        EmployeeService.updateTimeSheet(req.body)
+            .then(result => {res.send({status: 1, Message: "Success"})})
+            .catch(next);
     }
+    //  loginUser: function(req,res,next){
+    //     let userData;
+    //     UserService.loginUser(req.body)
+    //         .then( user =>{
+
+    //             userData = user;
+    //             return UserService.getUserWalletData(userData.user_id);
+                
+    //         })
+    //         .then(walletData=>{
+    //             userData.balance=walletData;
+    //             userData.status=true;
+    //             res.status(200);
+    //             res.send(userData);
+    // })
 };
 
 export default controller;
--- server/api/employee/employee.service.js
+++ server/api/employee/employee.service.js
@@ -3,13 +3,95 @@ import connection from '../../db/db-pg-connection.js';
 let EmployeeService = {
     getEmployees() {
         return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM public.employees', (err, res) => {
+            connection.query('SELECT * FROM public.employee', (err, res) => {
                 if (err)
                     reject(err);
                 else
                     resolve(res.rows);
             });
         });
+    },
+    // getWeeklyList() {
+    //     return new Promise((resolve, reject) => {
+    //         const text = 
+    //         const value = 
+    //         connection.query(text,value,(err, res) => {
+
+
+    //             if (err){  
+    //                 reject(err);
+    //             }
+    //             else{
+
+    //                 resolve(res.rows[0]);
+    //             }
+    //         });
+    //     });
+    // },
+
+    getTimeSheet(data) {
+        return new Promise((resolve, reject) => {
+            const addTime = new Date();
+            const query = {
+                text: "SELECT * FROM public.timesheet_management WHERE employee_id =$1 AND project_id =$2 AND date_for_timesheet =$3",
+                value: [data.employee_id, data.project_id, data.date_for_timesheet]
+
+            }
+
+
+
+            connection.query(query, (err, res) => {
+                if (err) {
+                    // console.log('errrrorrr', err);
+                    reject(err);
+                }
+                else {
+                    console.log("promises");
+                    resolve(res.rows[0]);
+                }
+
+            });
+
+        });
+    },
+
+    updateTimeSheet(array) {
+        let promises = [], promise;
+
+        array.forEach(data => {
+            promise = new Promise((resolve, reject) => {
+                 const addTime = new Date();
+                let queryString;
+
+                if(data && !data.id){
+                    queryString = {
+                        text: 'INSERT INTO public.timesheet_management (employee_id, project_id, activity_id, status, active_hours, date_for_timesheet, created_at, updated_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8)',
+                        values: [data.employee_id, data.project_id, data.activity_id, data.status, data.active_hours, addTime, addTime, addTime]
+                    };
+                }
+                else{
+                    queryString = {
+                        text: 'UPDATE public.timesheet_management SET activity_id = $1, status = $2, active_hours = $3, updated_at = $4 WHERE id=$5',
+                        values: [data.activity_id, data.status, data.active_hours, addTime, data.id]
+                    };
+                }
+                    
+                
+                    connection.query(queryString, (err, res) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve(res);
+                        }
+                    });
+
+            });
+
+            promises.push(promise);
+        });
+
+        return Promise.all(promises);
     }
 };
 
