075215d525b0c12afee2ad732edaf3ab53627533
urvashippl10 <urvashi.k@people10.com>
2017-08-05 05:36:17 +0530

    weeklyTS screen sample

--- client/starter-app/src/app/app-routing.module.ts
+++ client/starter-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { WeeklyTsComponent } from './pages/weekly-ts/weekly-ts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPageComponent
+  },
+  {
+    path: 'weekly-ts',
+    component: WeeklyTsComponent
   }
 ];
 
